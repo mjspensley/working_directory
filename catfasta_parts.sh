92259901e546817eddc8639bb78f64869cc808b7
mjspensley <mjspensley@proton.me>
2025-12-03 20:59:42 +0000

    Current working version of shell script to concatenate individual parts into one file, ready for analysis.

--- catfasta_parts.sh
+++ catfasta_parts.sh
@@ -1,32 +1,30 @@
 #!/bin/zsh
 
 # remove any pre-existing fasta files
-rm -f sample*.fasta wrk*.fasta
+rm -f sample*.fasta
 
 # for each sample out of A B C and D
-for i in A B C D; do
+for sample in A B C D; do
 
   # create new sample fasta file
-  touch sample${i}.fasta
-
+  touch sample${sample}_parts.fasta
+  
   # process each part (1, 2, 3) separately
   for part in 1 2 3; do
 
-    # add a header for each part
-    echo ">sample${i}_part${part}" >> sample${i}.fasta
-
-    # use sed to remove spaces and sample headers then grep to retrieve sequence and append to working fasta file
-    sed -e 's/ //g' -e 's/@sample._part_.//g' sample${i}*part${part}.FASTQ | grep '^[ATCGNYRWSKMDVHBXN-]*$' >> wrk${i}_part${part}.fasta
-
-    # remove empty lines
-    sed -i '' '/^$/d' wrk${i}_part${part}.fasta
-
-    # append working fasta file contents to sample fasta file
-    cat wrk${i}_part${part}.fasta >> sample${i}.fasta
+    # add header for this part
+    echo ">sample${sample}_part${part}" >> sample${sample}_parts.fasta
+    
+    # filter bases by phred score 20 (99% accuracy) using seqtk, substitute masked bases with N
+    # remove spaces and sample headers using sed, extract sequence using grep and append to sample fasta
+    # use fold to limit line length to 80 characters (typical fasta format)
+    seqtk seq -q 20 sample${sample}*part${part}.FASTQ | sed 's/[atcgnyrwskmdvhbxn]/N/g' \
+    | sed -e 's/ //g' -e 's/@sample._part_.//g' | grep '^[ATCGNYRWSKMDVHBXN-]*$' \
+    | fold -w 80 >> sample${sample}_parts.fasta
 
   done
 
-done
+  # remove empty lines
+  sed -i '' '/^$/d' sample${sample}_parts.fasta
 
-# remove working fasta file
-rm -f wrk*.fasta
+done
